bae1e87c9db177823e86ac933598bc490330736e
Momentum Robotics <momentumrobotics@gmail.com>
2023-01-10 12:25:04 -0800

    Vision Subsystem: Debug apriltag detection

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,6 +4,7 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.first.apriltag.AprilTagDetection;
@@ -13,6 +14,8 @@ import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
 import edu.wpi.first.cscore.MjpegServer;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoMode;
+import edu.wpi.first.cscore.VideoMode.PixelFormat;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.networktables.GenericSubscriber;
@@ -38,32 +41,39 @@ public class VisionSubsystem extends SubsystemBase {
     private Thread visionThread;
 
     private Mat currFrame = new Mat();
+    private Mat outFrame = new Mat();
     private Mat bwFrame = new Mat();
 
     public VisionSubsystem() {
         lifecam = new UsbCamera("Lifecam", 0);
+
+        lifecam.setVideoMode(PixelFormat.kMJPEG, 640, 480, 30);
+
         inputStream = new CvSink("opencv_Lifecam");
         inputStream.setSource(lifecam);
 
-        outputStream = new CvSource("VisionSubsystem", lifecam.getVideoMode());
+        outputStream = new CvSource("VisionSubsystem", PixelFormat.kMJPEG, 320, 240, 30);
         mjpegServer = new MjpegServer("serve_VisionSubsystem", 1181);
         mjpegServer.setSource(outputStream);
 
         shouldDetectAprilTags = MoShuffleboard.getInstance().detectAprilTagsSwitch.getEntry();
 
         AprilTagDetector.Config detectorConfig = new AprilTagDetector.Config();
-        detectorConfig.numThreads = 2;
+        detectorConfig.numThreads = 3;
+        detectorConfig.quadDecimate = 3;
         detectorConfig.quadSigma = 0;
 
         detector = new AprilTagDetector();
         detector.setConfig(detectorConfig);
 
+        detector.addFamily("tag16h5", 0);
+
         AprilTagPoseEstimator.Config poseEstimatorConfig = new AprilTagPoseEstimator.Config(
             0.1524,
-            843.613849,
-            841.550832,
-            309.407580,
-            213.981279
+            672.5873577443733,
+            673.3134462534064,
+            333.5730281629913,
+            226.2345964911072
         );
         poseEstimator = new AprilTagPoseEstimator(poseEstimatorConfig);
 
@@ -80,7 +90,8 @@ public class VisionSubsystem extends SubsystemBase {
                             drawDetection(detection, estimatedPose, currFrame);
                         }
                     }
-                    outputStream.putFrame(currFrame);
+                    Imgproc.resize(currFrame, outFrame, new Size(320, 240));
+                    outputStream.putFrame(outFrame);
                 }
             }
         });
@@ -93,25 +104,28 @@ public class VisionSubsystem extends SubsystemBase {
     }
 
     private void drawDetection(AprilTagDetection detection, Transform3d estimatedTransform, Mat outMat) {
+        final double fontSize = 1.1;
+        final int thickness = 1;
+
         for(int i = 0; i < 4; ++i) {
             Point prev = new Point(detection.getCornerX((i + 3) % 4), detection.getCornerY((i + 3) % 4));
             Point curr = new Point(detection.getCornerX(i), detection.getCornerY(i));
 
-            Imgproc.line(outMat, prev, curr, new Scalar(255, 0, 255), 2, 1);
+            Imgproc.line(outMat, prev, curr, new Scalar(255, 0, 255), thickness, 1);
         }
 
         String label = Integer.toString(detection.getId());
 
         Point textPoint = new Point(detection.getCornerX(3), detection.getCornerY(3)-5);
-        Imgproc.putText(outMat, label, textPoint, Imgproc.FONT_HERSHEY_PLAIN, 1.2, new Scalar(255, 0, 255), 2);
+        Imgproc.putText(outMat, label, textPoint, Imgproc.FONT_HERSHEY_PLAIN, fontSize, new Scalar(255, 0, 255), thickness);
 
         String translationTxt = String.format("(x:%.2f, y:%.2f, z:%.2f)", estimatedTransform.getX(), estimatedTransform.getY(), estimatedTransform.getZ());
         textPoint.x += 20;
-        Imgproc.putText(outMat, translationTxt, textPoint, Imgproc.FONT_HERSHEY_PLAIN, 1.2, new Scalar(0, 255, 0), 2);
+        Imgproc.putText(outMat, translationTxt, textPoint, Imgproc.FONT_HERSHEY_PLAIN, fontSize, new Scalar(0, 255, 0), thickness);
 
         Rotation3d rot = estimatedTransform.getRotation();
         String rotTxt = String.format("(r:%.2f, p:%.2f, y:%.2f)", rot.getX(), rot.getY(), rot.getZ());
         textPoint.y += 20;
-        Imgproc.putText(outMat, rotTxt, textPoint, Imgproc.FONT_HERSHEY_PLAIN, 1.2, new Scalar(0, 128, 255), 2);
+        Imgproc.putText(outMat, rotTxt, textPoint, Imgproc.FONT_HERSHEY_PLAIN, fontSize, new Scalar(0, 128, 255), thickness);
     }
 }
