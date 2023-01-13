4ca3c6edec2a4e897878e386f8d699806d554988
Jordan Powers <jordanpowers1227@gmail.com>
2023-01-13 10:15:34 -0800

    Use XboxController instead of LogitechF310
    The button and axis mappings were all wrong in the custom LogitechF310
    class. Also, it seems that the XboxController has the same mappings as
    the LogitechF310 controller this year, so we'll just use an instance of
    XboxController.

--- src/main/java/frc/robot/input/SingleControllerInput.java
+++ src/main/java/frc/robot/input/SingleControllerInput.java
@@ -2,18 +2,17 @@ package frc.robot.input;
 
 import static com.momentum4999.utils.Utils.*;
 
-import com.momentum4999.utils.controllers.LogitechF310;
-
+import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants;
 
 public class SingleControllerInput implements MoInput {
     private static final double DEADZONE = 0.1;
     private static final double CURVE = 2.5;
 
-    private final LogitechF310 controller;
+    private final XboxController controller;
 
     public SingleControllerInput(Constants.HIDPort port) {
-        this.controller = new LogitechF310(port.port);
+        this.controller = new XboxController(port.port);
     }
 
     @Override
