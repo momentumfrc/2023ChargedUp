9c7db6a0bb62e8874eaf0a0b749f1080409b9cd7
Jordan Powers <jordanpowers1227@gmail.com>
2023-01-16 13:31:07 -0800

    Use drive PID

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 import com.playingwithfusion.CANVenom;
+import com.playingwithfusion.CANVenom.ControlMode;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.GenericSubscriber;
@@ -12,6 +13,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.utils.MoPrefs;
 import frc.robot.utils.MoShuffleboard;
 import frc.robot.utils.VenomTunerAdapter;
 
@@ -28,8 +30,6 @@ public class DriveSubsystem extends SubsystemBase {
     private final PIDTuner rearLeftTuner = new PIDTuner("Drive BL", new VenomTunerAdapter(rearLeftMtr));
     private final PIDTuner rearRightTuner = new PIDTuner("Drive BR", new VenomTunerAdapter(rearRightMtr));
 
-    private final MecanumDrive drive = new MecanumDrive(frontLeftMtr, rearLeftMtr, frontRightMtr, rearRightMtr);
-
     private AHRS gyro = new AHRS(SerialPort.Port.kMXP);
 
     private Rotation2d initialHeading = gyro.getRotation2d();
@@ -41,22 +41,33 @@ public class DriveSubsystem extends SubsystemBase {
     private ComplexWidget doResetOrientation = MoShuffleboard.getInstance().settingsTab
         .add("Reset Drive Orientation", new InstantCommand(() -> { initialHeading = gyro.getRotation2d(); }));
 
+    private GenericSubscriber shouldDrivePID = MoShuffleboard.getInstance().settingsTab
+        .add("PID Drive", true)
+        .withWidget(BuiltInWidgets.kToggleSwitch).getEntry();
+
     public void driveCartesian(double fwdRequest, double leftRequest, double turnRequest) {
+        Rotation2d gyroAngle;
         if(shouldDriveFieldOriented.getBoolean(true)) {
-            driveCartesianFieldOriented(fwdRequest, leftRequest, turnRequest);
+            // TODO: get the Rotation2d from the odometry, not the gyro (so that it uses the AprilTags)
+            //       Example: this.odometry.getPoseMeters().getRotation();
+            gyroAngle = gyro.getRotation2d().minus(initialHeading);
         } else {
-            driveCartesianRobotOriented(fwdRequest, leftRequest, turnRequest);
+            gyroAngle = new Rotation2d();
+        }
+
+        var wheelSpeeds = MecanumDrive.driveCartesianIK(fwdRequest, leftRequest, turnRequest, gyroAngle);
+        double maxSpeedRpm = MoPrefs.maxDriveRpm.get();
+
+        if(shouldDrivePID.getBoolean(true)) {
+            frontLeftMtr.setCommand(ControlMode.SpeedControl, wheelSpeeds.frontLeft * maxSpeedRpm);
+            frontRightMtr.setCommand(ControlMode.SpeedControl, wheelSpeeds.frontRight * maxSpeedRpm);
+            rearLeftMtr.setCommand(ControlMode.SpeedControl, wheelSpeeds.rearLeft * maxSpeedRpm);
+            rearRightMtr.setCommand(ControlMode.SpeedControl, wheelSpeeds.rearRight * maxSpeedRpm);
+        } else {
+            frontLeftMtr.setCommand(ControlMode.Proportional, wheelSpeeds.frontLeft);
+            frontRightMtr.setCommand(ControlMode.Proportional, wheelSpeeds.frontRight);
+            rearLeftMtr.setCommand(ControlMode.Proportional, wheelSpeeds.rearLeft);
+            rearRightMtr.setCommand(ControlMode.Proportional, wheelSpeeds.rearRight);
         }
     }
-
-    public void driveCartesianFieldOriented(double fwdRequest, double leftRequest, double turnRequest) {
-        // TODO: get the Rotation2d from the odometry, not the gyro (so that it uses the AprilTags)
-        //       Example: this.odometry.getPoseMeters().getRotation();
-        drive.driveCartesian(fwdRequest, leftRequest, turnRequest, gyro.getRotation2d().minus(initialHeading));
-    }
-
-    public void driveCartesianRobotOriented(double fwdRequest, double leftRequest, double turnRequest) {
-        drive.driveCartesian(fwdRequest, leftRequest, turnRequest);
-    }
-
 }
