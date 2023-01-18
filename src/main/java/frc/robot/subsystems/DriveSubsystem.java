1e4cd2e6bbe426327d12d8416f74ff5065ae8db4
Jordan Powers <jordanpowers1227@gmail.com>
2023-01-17 21:02:56 -0800

    Use PID to maintain robot heading

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems;
 
+import java.io.File;
+
 import com.kauailabs.navx.frc.AHRS;
 import com.playingwithfusion.CANVenom;
 import com.playingwithfusion.CANVenom.ControlMode;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.GenericSubscriber;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
@@ -13,6 +16,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.utils.MoPIDF;
 import frc.robot.utils.MoPrefs;
 import frc.robot.utils.MoShuffleboard;
 import frc.robot.utils.VenomTunerAdapter;
@@ -25,14 +29,18 @@ public class DriveSubsystem extends SubsystemBase {
     private final CANVenom rearLeftMtr = new CANVenom(Constants.DRIVE_LEFT_REAR.address);
     private final CANVenom rearRightMtr = new CANVenom(Constants.DRIVE_RIGHT_REAR.address);
 
-    private final PIDTuner frontLeftTuner = new PIDTuner("Drive FL", new VenomTunerAdapter(frontLeftMtr));
-    private final PIDTuner frontRightTuner = new PIDTuner("Drive FR", new VenomTunerAdapter(frontRightMtr));
-    private final PIDTuner rearLeftTuner = new PIDTuner("Drive BL", new VenomTunerAdapter(rearLeftMtr));
-    private final PIDTuner rearRightTuner = new PIDTuner("Drive BR", new VenomTunerAdapter(rearRightMtr));
+    private final PIDTuner frontLeftTuner;
+    private final PIDTuner frontRightTuner;
+    private final PIDTuner rearLeftTuner;
+    private final PIDTuner rearRightTuner;
+
+    private final MoPIDF headingController = new MoPIDF();
+    private final PIDTuner headingTuner;
 
     private AHRS gyro = new AHRS(SerialPort.Port.kMXP);
 
     private Rotation2d initialHeading = gyro.getRotation2d();
+    private Rotation2d maintainHeading = gyro.getRotation2d();
 
     private GenericSubscriber shouldDriveFieldOriented = MoShuffleboard.getInstance().settingsTab
         .add("Field-Oriented Drive", true)
@@ -45,17 +53,41 @@ public class DriveSubsystem extends SubsystemBase {
         .add("PID Drive", true)
         .withWidget(BuiltInWidgets.kToggleSwitch).getEntry();
 
-    public void driveCartesian(double fwdRequest, double leftRequest, double turnRequest) {
-        Rotation2d gyroAngle;
-        if(shouldDriveFieldOriented.getBoolean(true)) {
-            // TODO: get the Rotation2d from the odometry, not the gyro (so that it uses the AprilTags)
-            //       Example: this.odometry.getPoseMeters().getRotation();
-            gyroAngle = gyro.getRotation2d().minus(initialHeading);
-        } else {
-            gyroAngle = new Rotation2d();
+    private GenericSubscriber shouldHeadingPID = MoShuffleboard.getInstance().settingsTab
+        .add("Keep Heading", true)
+        .withWidget(BuiltInWidgets.kToggleSwitch).getEntry();
+
+    public DriveSubsystem() {
+        PIDTuner.PIDTunerSettings settings = new PIDTuner.PIDTunerSettings();
+        if(RobotBase.isReal()) {
+            settings.saveValuesLocation = new File("/home/lvuser/pid_constants.ini");
         }
 
-        var wheelSpeeds = MecanumDrive.driveCartesianIK(fwdRequest, leftRequest, turnRequest, gyroAngle);
+        frontLeftTuner = new PIDTuner("Drive FL", new VenomTunerAdapter(frontLeftMtr), settings);
+        frontRightTuner = new PIDTuner("Drive FR", new VenomTunerAdapter(frontRightMtr), settings);
+        rearLeftTuner = new PIDTuner("Drive BL", new VenomTunerAdapter(rearLeftMtr), settings);
+        rearRightTuner = new PIDTuner("Drive BR", new VenomTunerAdapter(rearRightMtr), settings);
+        headingTuner = new PIDTuner("Drive Heading", headingController, settings);
+    }
+
+    public void driveCartesian(double fwdRequest, double leftRequest, double turnRequest) {
+        // TODO: get the Rotation2d from the odometry, not the gyro (so that it uses the AprilTags)
+        //       Example: this.odometry.getPoseMeters().getRotation();
+        Rotation2d currentHeading = gyro.getRotation2d();
+        Rotation2d fieldOrientedDriveAngle;
+        if(shouldDriveFieldOriented.getBoolean(true)) {
+            fieldOrientedDriveAngle = currentHeading.minus(initialHeading);
+        } else {
+            fieldOrientedDriveAngle = new Rotation2d();
+        }
+
+        if(turnRequest != 0) {
+            maintainHeading = currentHeading;
+        } else if(shouldHeadingPID.getBoolean(true)) {
+            turnRequest = headingController.calculate(currentHeading.getRadians(), maintainHeading.getRadians());
+        }
+
+        var wheelSpeeds = MecanumDrive.driveCartesianIK(fwdRequest, leftRequest, turnRequest, fieldOrientedDriveAngle);
         double maxSpeedRpm = MoPrefs.maxDriveRpm.get();
 
         if(shouldDrivePID.getBoolean(true)) {
