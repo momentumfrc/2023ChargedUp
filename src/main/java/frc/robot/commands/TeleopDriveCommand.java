c1b25d5832f66ee020336b1500ba8c79582f6656
Momentum Robotics <momentumrobotics@gmail.com>
2023-01-23 18:49:47 -0800

    Remove slew rate limiting

--- src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -5,29 +5,14 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.input.MoInput;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.utils.MoPrefs;
-import frc.robot.utils.MoPrefs.Pref;
 
 public class TeleopDriveCommand extends CommandBase {
     private final DriveSubsystem drive;
     private final MoInput input;
 
-    private Pref<Double> rampTime = MoPrefs.driveRampTime;
-
-    private SlewRateLimiter fwdLimiter;
-    private SlewRateLimiter leftLimiter;
-    private SlewRateLimiter turnLimiter;
-
     public TeleopDriveCommand(DriveSubsystem drive, MoInput input) {
         this.drive = drive;
         this.input = input;
-
-        rampTime.subscribe(rampTime -> {
-            double slewRate = 1.0 / rampTime;
-
-            fwdLimiter = new SlewRateLimiter(slewRate);
-            leftLimiter = new SlewRateLimiter(slewRate);
-            turnLimiter = new SlewRateLimiter(slewRate);
-        }, true);
 
         addRequirements(drive);
     }
@@ -44,10 +29,6 @@ public class TeleopDriveCommand extends CommandBase {
             leftRequest *= slowSpeed;
             turnRequest *= slowSpeed;
         }
-
-        fwdRequest = fwdLimiter.calculate(fwdRequest);
-        leftRequest = leftLimiter.calculate(leftRequest);
-        turnRequest = turnLimiter.calculate(turnRequest);
 
         drive.driveCartesian(fwdRequest, leftRequest, turnRequest);
     }
