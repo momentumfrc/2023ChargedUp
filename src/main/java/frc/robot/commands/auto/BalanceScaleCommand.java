3f50efb2bd2d70e6a8359cd2c1b2cee1c8327b16
Jordan Powers <jordanpowers1227@gmail.com>
2023-02-09 18:59:57 -0800

    Add bang-bang controller for scale balancing

---
+++ src/main/java/frc/robot/commands/auto/BalanceScaleCommand.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.auto;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+/**
+ * A simple bang-bang controller to automatically level the scale.
+ */
+public class BalanceScaleCommand extends CommandBase {
+    private static final double MOVE_SPEED = 0.1;
+    private static final double LEVEL_DEFINITION = 2.5; // degrees
+
+    private final DriveSubsystem drive;
+    private final AHRS navx;
+
+    public BalanceScaleCommand(DriveSubsystem drive, AHRS navx) {
+        this.drive = drive;
+        this.navx = navx;
+
+        addRequirements(drive);
+    }
+
+    @Override
+    public void execute() {
+        // TODO: Verify if we need pitch or roll here
+        double pitch = navx.getPitch();
+
+        double moveRequest = 0;
+        if(pitch < -LEVEL_DEFINITION) {
+            moveRequest = -MOVE_SPEED;
+        } else if(pitch > LEVEL_DEFINITION) {
+            moveRequest = MOVE_SPEED;
+        }
+
+        drive.driveCartesian(moveRequest, 0, 0);
+    }
+
+}
