b8a0b98eecae000a2f5b9ea32f25e8e0d5d019c5
Momentum Robotics <momentumrobotics@gmail.com>
2023-03-06 19:34:16 -0800

    Fix Can IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,10 +35,10 @@ public class Constants {
     public static final CANAddress DRIVE_LEFT_REAR = new CANAddress(4);
     public static final CANAddress DRIVE_RIGHT_FRONT = new CANAddress(1);
     public static final CANAddress DRIVE_RIGHT_REAR = new CANAddress(5);
-    public static final CANAddress ARM_SHOULDER_LEFT = new CANAddress(2); // FIXME: Correct CAN ID
-    public static final CANAddress ARM_SHOULDER_RIGHT = new CANAddress(7); // FIXME: Correct CAN ID
-    public static final CANAddress ARM_WRIST = new CANAddress(6); // FIXME: Correct CAN ID
-    public static final CANAddress INTAKE_ROLLER = new CANAddress(1); // FIXME: Correct CAN ID
+    public static final CANAddress ARM_SHOULDER_LEFT = new CANAddress(2);
+    public static final CANAddress ARM_SHOULDER_RIGHT = new CANAddress(7);
+    public static final CANAddress ARM_WRIST = new CANAddress(9);
+    public static final CANAddress INTAKE_ROLLER = new CANAddress(1);
 
     public static final HIDPort F310 = new HIDPort(0);
 
